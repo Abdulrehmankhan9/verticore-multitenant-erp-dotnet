93c478bce2847050b0c33b3a912ad15c44952f47
Abdulrehmankhan9 <abdulrehmankhan.ak91@gmail.com>
2026-09-07 08:07:11 +0500

    DTOs complete

---
+++ VertiCore.Application/VertiCore.Application/DTOs/Invoice/InvoiceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VertiCore.Application.DTOs.Invoice
+{
+    internal class InvoiceDto
+    {
+    }
+}
