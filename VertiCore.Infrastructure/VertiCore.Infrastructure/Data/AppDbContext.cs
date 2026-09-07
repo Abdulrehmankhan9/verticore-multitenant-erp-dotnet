467865d88df799c9a89e19deae32880b5d0fe080
Abdulrehmankhan9 <abdulrehmankhan.ak91@gmail.com>
2026-09-07 08:26:27 +0500

    Add AppDbContext with DbSets

---
+++ VertiCore.Infrastructure/VertiCore.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using VertiCore.Domain.Entities;
+
+namespace VertiCore.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tenant> Tenants { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public DbSet<AuditLog> AuditLogs { get; set; }
+    }
+}
