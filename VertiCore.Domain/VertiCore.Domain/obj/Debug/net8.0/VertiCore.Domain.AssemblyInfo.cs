81502ba89e84f3aba16eea37c072f04dc77a9a03
Abdulrehmankhan9 <abdulrehmankhan.ak91@gmail.com>
2026-09-20 02:21:41 +0500

    Reorganize interfaces into Services and Repositories subfolders - build fixed2

--- VertiCore.Domain/VertiCore.Domain/obj/Debug/net8.0/VertiCore.Domain.AssemblyInfo.cs
+++ VertiCore.Domain/VertiCore.Domain/obj/Debug/net8.0/VertiCore.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VertiCore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb04f29f5d2bb6664517ecdcc57cb5447d906a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be367667877dfdc51640d3bbe244c37f923351b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VertiCore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VertiCore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
